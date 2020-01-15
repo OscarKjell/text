4c45b6e0a4c3554147eb501eb3ec3e9df7e5c76a
Oscar Kjell <oscar_kjell@hotmail.com>
2020-01-15 15:23:33 +0100

    change applysemrep_plot

--- R/0_globals_settings.R
+++ R/0_globals_settings.R
@@ -3,5 +3,5 @@
 # https://community.rstudio.com/t/how-to-solve-no-visible-binding-for-global-variable-note/28887
 utils::globalVariables(c("token_index", "layer_index",
                          "SS", "p_values.x", "p_values.y", "words", "n.x",
-                         "addEqualNrNArows", "applysemrep_plot", "single_word_embeddings_df",
+                         "addEqualNrNArows", "applysemrep", "single_word_embeddings_df",
                          "guide_legend"))
