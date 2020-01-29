82aa96e5730825113d748aca4030786597e96b99
Oscar Kjell <oscar_kjell@hotmail.com>
2020-01-29 07:37:28 +0100

    Omitting rows when NAs in y in training.

--- R/2_textTrain.R
+++ R/2_textTrain.R
@@ -32,16 +32,18 @@
 #' @param preProcessThresh Preprocessing threshold.
 #' @param methodCor Type of correlation used in evaluation.
 #' @param describe_model Input text to describe your model.
-#' @param ... Arguments from caret::createFolds, caret::trainControl and caret::train.
+#' @param ... Arguments fom caret::createFolds, caret::trainControl and caret::train.
 #' @return A correlation between predicted and observed values; as well as predicted values.
 #' @examples
 #' wordembeddings <- wordembeddings4_10
 #' ratings_data <- sq_data_tutorial4_10
 #' wordembeddings <- textTrain(wordembeddings$harmonytext, ratings_data$hilstotal, nrFolds_k=2)
 #' @seealso see \code{\link{textTrainLists}} \code{\link{textTtest}}
-#' @importFrom stats cor.test
+#' @importFrom stats cor.test na.omit
+#' @importFrom dplyr select starts_with
 #' @export
-
+#x <- wordembeddings4_10$satisfactiontexts
+#y <- sq_data_tutorial4_10$hilstotal
 textTrain <- function(x, y,  nrFolds_k=10, methodTrain = "ridge", preProcessTrain = 'pca', preProcessThresh = 0.95, methodCor = "pearson", describe_model = "Describe the model and share it with others", ...){
   # Preparing a df for training: Import data if x is character or use semreps if it has already been imported
   if (is.character(x) == TRUE) {
@@ -56,17 +58,21 @@ textTrain <- function(x, y,  nrFolds_k=10, methodTrain = "ridge", preProcessTrai
   }
   # Remove unnecassary columns
   df3 <- subset(df2, select=-c(1:5))
+  df3 <- dplyr::select(df3, -dplyr::starts_with("name"))
+
+  # Remove rows with NA
+  df3 <- stats::na.omit(df3)
 
   ## Training
   # Creating folds for outer cross-validation, so that the data is split into 90% for inner cv, and 10% for testing with that particular semrep dimesion solution; see solution from https://stats.stackexchange.com/questions/125843/outer-crossvalidation-cycle-in-caret-package-r
   ytrain_df3 <- c(1:nrow(df3)) # number of observation that should be split in to the number of folds.
   ntrain <- length(ytrain_df3)
   # define folds
-  cv_folds <- caret::createFolds(ytrain_df3, k = nrFolds_k, list = TRUE, returnTrain = TRUE, ...)
+  cv_folds <- caret::createFolds(ytrain_df3, k = nrFolds_k, list = TRUE, returnTrain = TRUE) # , ...
   # define training control
-  train_control <- caret::trainControl(method="cv", index = cv_folds, savePredictions = 'final', ...)
-  # train the model
-  model <- caret::train(y~., data=df3, trControl=train_control, method= methodTrain, preProcess = preProcessTrain, thresh = preProcessThresh, ...)
+  train_control <- caret::trainControl(method="cv", index = cv_folds, savePredictions = 'final') #, ...
+  # train the model help(train)
+  model <- caret::train(y~., data=df3, trControl=train_control, method= methodTrain, preProcess = preProcessTrain, thresh = preProcessThresh, na.action="na.omit")#, ...)
 
   # Describe model; adding user's-description + the name of the x and y
   #describe_model_detail <- c(describe_model, paste(names(x)), paste(names(y)))
