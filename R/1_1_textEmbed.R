b4bb1e8c98b1d332b5c2711a8e07c78209dabbc6
Oscar Kjell <oscarkjell@Oscars-MacBook-Pro-2.local>
2020-07-08 13:48:49 +0200

    changed to relative pathway

--- R/1_1_textEmbed.R
+++ R/1_1_textEmbed.R
@@ -330,7 +330,8 @@ textHuggingFace <- function(x,
                             model_class = NULL) {
 
   # Run python file with HunggingFace interface to state-of-the-art transformers
-  reticulate::source_python("~/inst/python/huggingface_Interface3.py")
+  # reticulate::source_python("~/inst/python/huggingface_Interface3.py")
+  reticulate::source_python(system.file("inst/python", "huggingface_Interface3.py", package="text"))
 
   # Setting up the specifics of the models; the parameters for HuggingFace.
   if(model == "bert-base-uncased"){
