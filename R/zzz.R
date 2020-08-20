95a7d10a9fccd8c005473c57af10e8455383ecd3
Oscar Kjell <oscar_kjell@hotmail.com>
2020-08-20 08:39:39 +0200

    increased version

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,7 @@
     warning("The text package requires running R on a 64-bit systems as it is dependent on torch from ptyhon; and you're system is not 64-bit.")
   }
   packageStartupMessage(
-    crayon::green("This is text (version 0.8.60). \n"),
+    crayon::green("This is text (version 0.8.70). \n"),
     crayon::blue("Newer versions may have updated default settings to reflect current understandings of the state-of-the-art.")
   )
 }
