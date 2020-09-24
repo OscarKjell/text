4c2b3d3e4de23a0792c979e7133fb3ce0e342b7a
Salvatore Giorgi <sal.giorgi@gmail.com>
2020-09-24 11:55:12 -0400

    break up sentences if longer than max tokens

--- inst/python/huggingface_Interface3.py
+++ inst/python/huggingface_Interface3.py
@@ -10,13 +10,15 @@ import numpy as np
 
 #TODO:#dictionary of pretrained weights to models
 def hgTransformerGetEmbedding(text_strings, #list of strings
-                              model = 'bert-base-uncased',
+                              pretrained_weights = 'bert-base-uncased',
                               layers = 'all',  #all or a list of layers to keep
                               return_tokens=True):
 
-    config = AutoConfig.from_pretrained(model, output_hidden_states=True)
-    tokenizer = AutoTokenizer.from_pretrained(model)
-    transformer_model = AutoModel.from_pretrained(model, config=config)
+    config = AutoConfig.from_pretrained(pretrained_weights, output_hidden_states=True)
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_weights)
+    transformer_model = AutoModel.from_pretrained(pretrained_weights, config=config)
+    
+    maxTokensPerSeg = tokenizer.max_len_sentences_pair//2
 
     ##Check and Adjust Input Typs
     if not isinstance(text_strings, list):
@@ -34,15 +36,34 @@ def hgTransformerGetEmbedding(text_strings, #list of strings
         input_ids = tokenizer.encode(text_string, add_special_tokens=True)
         if return_tokens:
             tokens = tokenizer.convert_ids_to_tokens(input_ids)
+        
+        if len(input_ids) > maxTokensPerSeg:
+            this_embs, this_toks = [], []
+            for sent_str in sent_tokenize(text_string):
+                input_ids = tokenizer.encode(sent_str, add_special_tokens=True)
+                if return_tokens:
+                    tokens = tokenizer.convert_ids_to_tokens(input_ids)
+                
+                with torch.no_grad():
+                    hidden_states = transformer_model(torch.tensor([input_ids]))[2]
+                    if layers != 'all': 
+                        hidden_states = [hidden_states[l] for l in layers]
+                    hidden_states = [h.tolist() for h in hidden_states]
+                    this_embs.extend(hidden_states)
+                    if return_tokens:
+                        this_toks.extend(tokens)
+            all_embs.append(this_embs)
+            all_toks.append(this_toks)
 
-        with torch.no_grad():
-            hidden_states = transformer_model(torch.tensor([input_ids]))[2]
-            if layers != 'all': 
-                hidden_states = [hidden_states[l] for l in layers]
-            hidden_states = [h.tolist() for h in hidden_states]
-            all_embs.append(hidden_states)
-            if return_tokens:
-                all_toks.append(tokens)
+        else:
+            with torch.no_grad():
+                hidden_states = transformer_model(torch.tensor([input_ids]))[2]
+                if layers != 'all': 
+                    hidden_states = [hidden_states[l] for l in layers]
+                hidden_states = [h.tolist() for h in hidden_states]
+                all_embs.append(hidden_states)
+                if return_tokens:
+                    all_toks.append(tokens)
 
     if return_tokens:
         return all_embs, all_toks
