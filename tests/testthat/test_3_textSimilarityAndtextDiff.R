76913d69381860c004f8ac68028cf56d6665cc7d
Oscar Kjell <oscar_kjell@hotmail.com>
2021-03-22 17:58:35 +0100

    increased number of permutations in test

--- tests/testthat/test_3_textSimilarityAndtextDiff.R
+++ tests/testthat/test_3_textSimilarityAndtextDiff.R
@@ -33,7 +33,7 @@ test_that("textSimilarityTest paired results in list with numeric output", {
   skip_on_cran()
 
   test_diff_results <- textSimilarityTest(wordembeddings4$harmonytexts, wordembeddings4$satisfactiontexts,
-    method = "paired", Npermutations = 10, N_cluster_nodes = 1
+    method = "paired", Npermutations = 100, N_cluster_nodes = 1
   )
 
   expect_that(test_diff_results, is_a("list"))
@@ -44,7 +44,7 @@ test_that("textSimilarityTest paired results in list with numeric output", {
   skip_on_cran()
 
   expect_error(textSimilarityTest(wordembeddings4$harmonytexts, wordembeddings4$satisfactiontexts[1:39, ],
-    method = "paired", Npermutations = 10, N_cluster_nodes = 1
+    method = "paired", Npermutations = 100, N_cluster_nodes = 1
   ))
 })
 
@@ -52,7 +52,7 @@ test_that("textSimilarityTest unpaired results in list with numeric output", {
   skip_on_cran()
 
   test_diff_results <- textSimilarityTest(wordembeddings4$harmonytexts, wordembeddings4$satisfactiontexts,
-    method = "unpaired", Npermutations = 10, N_cluster_nodes = 1
+    method = "unpaired", Npermutations = 100, N_cluster_nodes = 1
   )
 
   expect_that(test_diff_results, is_a("list"))
@@ -64,7 +64,7 @@ test_that("textSimilarityTest unpaired results in list with numeric output", {
   skip_on_cran()
 
   test_diff_results <- textSimilarityTest(wordembeddings4$harmonytexts, wordembeddings4$satisfactiontexts,
-                                          method = "unpaired", Npermutations = 10, N_cluster_nodes = 1,
+                                          method = "unpaired", Npermutations = 100, N_cluster_nodes = 1,
                                           output.permutations = FALSE
   )
 
