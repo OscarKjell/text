0971ac12cba29d8a56ad3d1eba00a3d89f8284d3
Oscar Kjell <oscar_kjell@hotmail.com>
2021-11-27 07:38:18 +0100

    installing miniconda through text with opensll

--- R/0_0_text_install.R
+++ R/0_0_text_install.R
@@ -246,7 +246,7 @@ process_textrpp_installation_conda <- function(conda,
   packages <- rpp_version
 
   # OK: Install OpenSSL for conda using NOT pip
-  reticulate::conda_install(envname, packages = "openssl-1.1.1k", pip = FALSE, conda = conda)
+  reticulate::conda_install(envname, packages = "openssl-1.1.1l-h2bbff1b_0.tar.bz2", pip = FALSE, conda = conda)
   #reticulate::conda_install(envname, packages = "sll", pip = FALSE, conda = conda)
   #reticulate::conda_install(envname, packages = "pyopenssl", pip = FALSE, conda = conda)
   reticulate::conda_install(envname, packages, pip = pip, conda = conda)
