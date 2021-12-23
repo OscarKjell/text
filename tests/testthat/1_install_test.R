9551ba7c552141c35272fe4ebfe915c092650ae5
Oscar Kjell <oscar_kjell@hotmail.com>
2021-12-23 14:03:14 +0100

    installa test

---
+++ tests/testthat/1_install_test.R
@@ -0,0 +1,15 @@
+library(testthat)
+library(text)
+
+test_that("installing text", {
+
+  #help(textrpp_install)
+  textrpp_install(prompt = FALSE)
+
+  #help(textrpp_initialize)
+  #.rs.restartR()
+  textrpp_initialize(textEmbed_test=FALSE)
+
+  text_test <- textEmbed("hello")
+  expect_that(ncol(text_test[[1]]), equals(1536))
+})
--- tests/testthat/notrun/test_99_textEmbed.R
+++ tests/testthat/notrun/test_99_textEmbed.R
@@ -19,16 +19,6 @@ skip_if_no_torch <- function() {
 }
 
 
-test_that("installing text", {
-
-  #help(textrpp_install)
-  textrpp_install(prompt = FALSE)
-
-  #help(textrpp_initialize)
-  #.rs.restartR()
-  textrpp_initialize(textEmbed_test=TRUE)
-})
-
 test_that("textEmbedLayerAggregation 'all': layer =  aggregate_tokens = 'mean' produces aggregated word embeddings", {
   skip_on_cran()
 
