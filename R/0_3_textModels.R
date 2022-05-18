e432537391174d7d02e382bcffa8755da13868b2
Oscar Kjell <oscar_kjell@hotmail.com>
2022-05-18 19:38:22 +0200

    Add textModels and textModelsRemove to its own file, and adding relevant global bindings

---
+++ R/0_3_textModels.R
@@ -0,0 +1,54 @@
+#' Check downloaded, available models.
+#' @return List of names of models and tokenizers
+#' @examples
+#' \donttest{
+#' textModels()
+#' }
+#' @seealso see \code{\link{textModelsRemove}}
+#' @export
+textModels <- function(){
+  reticulate::source_python(system.file("python",
+                                        "textModelPy.py",
+                                        # envir = NULL,
+                                        package = "text",
+                                        mustWork = TRUE
+  ))
+
+  models <- textModelsPy()
+
+  names(models) <- c("Downloaded_models", "Downloaded_tokenizers")
+
+  return (models)
+}
+
+
+#' Delete a specified model and model associated files.
+#' @param target_model Character string of the model name that you want to delete.
+#' @return Confirmation whether the model has been deleted.
+#' @examples
+#' \donttest{
+#' textModelsRemove("name-of-model")
+#' }
+#' @seealso see \code{\link{textModels}}
+#' @export
+textModelsRemove <- function(target_model){
+  reticulate::source_python(system.file("python",
+                                        "textModelPy.py",
+                                        # envir = NULL,
+                                        package = "text",
+                                        mustWork = TRUE
+  ))
+
+  # Check that target_models exist among existing models
+  models <- textModels()
+  if(target_model %in% models$Downloaded_models == FALSE){
+    stop(paste("Model name error. ", target_model, " was not found, and could not be deleted", sep=""))
+  }
+
+  # Delete the model
+  textModelsRMPy(target_model)
+
+  message(colourise(
+    paste(target_model, " was successfully deleted.", sep=""),
+    fg = "green", bg = NULL))
+}
