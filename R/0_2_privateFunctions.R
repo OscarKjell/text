3e8871b1b87a8d8d2fd25563fa1eea16b768730d
Oscar Kjell <oscar_kjell@hotmail.com>
2022-07-06 13:38:05 +0200

    new helper function.

--- R/0_2_privateFunctions.R
+++ R/0_2_privateFunctions.R
@@ -57,6 +57,55 @@ p_value_comparing_with_Null <- function(Observedresult,
   return(p_value)
 }
 
+# Help functions
+#' Add numeric variables to word embeddings
+#' @param word_embeddings Word embeddings to add variables to.
+#' @param data Variables to be added to the word embeddings before training.
+#' @param first Option to add variables before or after all word embeddings.
+#' @return Object of word embeddings with added variables referred to as Dim0X_names.
+#' @examples
+#' \donttest{
+#' embeddings_with_variables <- add_variables_to_we(word_embeddings_4[1],
+#' Language_based_assessment_data_8[c(6,7)],
+#' first = TRUE)
+#' }
+#' @importFrom dplyr bind_cols
+#' @importFrom purrr map
+#' @importFrom tibble as_tibble
+#' @noRd
+add_variables_to_we <- function(word_embeddings, data, first=TRUE){
+
+  # Add Names to new Variables
+  colnames(data) <- paste("Dim0", 1:ncol(data), "_", colnames(data), sep="")
+
+  # Remove single_we if exist
+  if(is.list(word_embeddings$singlewords_we) == TRUE){
+    word_embeddings$singlewords_we <- NULL
+  }
+
+  # If list of word embeddings
+  if(!is.data.frame(word_embeddings)){
+
+    # Add first
+    if(first == TRUE) ratings_embeddings <- purrr::map(word_embeddings, ~cbind(data, .x))
+    # Add last
+    if(first == FALSE) ratings_embeddings <- purrr::map(word_embeddings, ~cbind(.x, data))
+
+    ratings_embeddings_tibble <- lapply(ratings_embeddings, tibble::as_tibble)
+  }
+
+  # If list of word embeddings
+  if(is.data.frame(word_embeddings)){
+
+    # Add first
+    if(first == TRUE) ratings_embeddings_tibble <- dplyr::bind_cols(data, word_embeddings)
+    # Add last
+    if(first == FALSE) ratings_embeddings_tibble <- dplyr::bind_cols(word_embeddings, data)
+  }
+
+  return(ratings_embeddings_tibble)
+}
+
 
 #' Cohen's D effect size
 #'
