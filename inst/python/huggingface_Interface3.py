f6118103000e5889938c7d42d9cab5d76020716a
Salvatore Giorgi <sgiorgi@gmail.com>
2022-07-08 16:35:07 -0400

    added hgTransformerGetSentiment python method

--- inst/python/huggingface_Interface3.py
+++ inst/python/huggingface_Interface3.py
@@ -5,6 +5,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import torch
 from transformers import AutoConfig, AutoModel, AutoTokenizer
 from transformers.utils import logging
+from transformers import pipeline
 import numpy as np
 
 import nltk
@@ -18,6 +19,165 @@ from nltk.tokenize import sent_tokenize
 import os, sys
 
 
+def set_logging_level(logging_level):
+    """
+    Set the logging level
+
+    Parameters
+    ----------
+    logging_level : str
+        set logging level, options: critical, error, warning, info, debug
+    """
+    logging_level = logging_level.lower()
+    # default level is warning, which is in between "error" and "info"
+    if logging_level not in ['warn', 'warning']:
+        if logging_level == "critical":
+            logging.set_verbosity_critical()
+        elif logging_level == "error":
+            logging.set_verbosity_error()
+        elif logging_level == "info":
+            logging.set_verbosity_info()
+        elif logging_level == "debug":
+            logging.set_verbosity_debug()
+        else:
+            print("Warning: Logging level {l} is not an option.".format(l=logging_level))
+            print("\tUse one of: ")
+
+def set_tokenizer_parallelism(tokenizer_parallelism):
+    if tokenizer_parallelism:
+        os.environ["TOKENIZERS_PARALLELISM"] = "true"
+    else:
+        os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+def get_device(device):
+    """
+    Get device and device number
+
+    Parameters
+    ----------
+    device : str
+        name of device: 'cpu', 'gpu', or 'gpu:k' where k is a specific device number
+
+    Returns
+    -------
+    device : str
+        final selected device name
+    device_num : int
+        device number, -1 for CPU
+    """
+    device = device.lower()
+    if not device.startswith('cpu') and not device.startswith('gpu') and not device.startswith('cuda'):
+        print("device must be 'cpu', 'gpu', 'cuda', or of the form 'gpu:k' or 'cuda:k'")
+        print("\twhere k is an integer value for the device")
+        print("Trying CPUs")
+        device = 'cpu'
+    
+    device_num = -1
+    if device != 'cpu':
+        attached = False
+        if torch.cuda.is_available():
+            if device == 'gpu' or device == 'cuda': 
+                # assign to first gpu device number
+                device_num = list(range(torch.cuda.device_count()))[0]
+                attached = True
+            else: # assign to specific gpu device number
+                try:
+                    device_num = int(device.split(":")[-1])
+                    attached = True
+                except:
+                    pass
+        if not attached:
+            print("Unable to use CUDA (GPU), using CPU")
+            device = "cpu"
+            device_num = -1
+
+    return device, device_num
+
+def get_model(model):
+    """
+    Get model and tokenizer from model string
+
+    Parameters
+    ----------
+    model : str
+        shortcut name for Hugging Face pretained model
+        Full list https://huggingface.co/transformers/pretrained_models.html
+    
+    Returns
+    -------
+    config
+    tokenizer
+    model
+    """
+    if "megatron-bert" in model:
+        try:
+            from transformers import BertTokenizer, MegatronBertForMaskedLM
+        except:
+            print("WARNING: You must install transformers>4.10 to use MegatronBertForMaskedLM")
+            print("\tPlease try another model.")
+            sys.exit()
+
+        config = AutoConfig.from_pretrained(model, output_hidden_states=True)
+        if "megatron-bert-cased" in model:
+            tokenizer = BertTokenizer.from_pretrained('nvidia/megatron-bert-cased-345m')
+        else:
+            tokenizer = BertTokenizer.from_pretrained('nvidia/megatron-bert-uncased-345m')
+        transformer_model = MegatronBertForMaskedLM.from_pretrained(model, config=config)
+    else:
+        config = AutoConfig.from_pretrained(model, output_hidden_states=True)
+        tokenizer = AutoTokenizer.from_pretrained(model)
+        transformer_model = AutoModel.from_pretrained(model, config=config)
+    return config, tokenizer, transformer_model
+
+def hgTransformerGetSentiment(text_strings,
+                            model = '',
+                            device = 'cpu',
+                            tokenizer_parallelism = False,
+                            logging_level = 'warning'):
+    """
+    Simple interface getting sentiment scores from text
+
+    Parameters
+    ----------
+    text_strings : list
+        list of strings, each is embedded separately
+    model : str
+        shortcut name for Hugging Face pretained model
+        Full list https://huggingface.co/transformers/pretrained_models.html
+    device : str
+        name of device: 'cpu', 'gpu', or 'gpu:k' where k is a specific device number
+    tokenizer_parallelism :  bool
+        something
+    logging_level : str
+        set logging level, options: critical, error, warning, info, debug
+
+    Returns
+    -------
+    sentiment_scores : list
+        list of dictionaries with sentiment scores and labels
+    """
+
+    set_logging_level(logging_level)
+    set_tokenizer_parallelism(tokenizer_parallelism)
+    device, device_num = get_device(device)
+
+    # check and adjust input types
+    if not isinstance(text_strings, list):
+        text_strings = [text_strings]
+
+    if model:
+        config, tokenizer, transformer_model = get_model(model)
+        if device_num > 0:
+            sentiment_pipeline = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer, device=device_num)
+        else:
+            sentiment_pipeline = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+    else:
+        if device_num > 0:
+            sentiment_pipeline = pipeline("sentiment-analysis", device=device_num)
+        else:
+            sentiment_pipeline = pipeline("sentiment-analysis")
+    sentiment_scores = sentiment_pipeline(text_strings)
+    return sentiment_scores
 
 def hgTransformerGetEmbedding(text_strings,
                               model = 'bert-large-uncased',
@@ -62,77 +222,26 @@ def hgTransformerGetEmbedding(text_strings,
         tokenized version of text_strings
     """
 
-    logging_level = logging_level.lower()
-    # default level is warning, which is in between "error" and "info"
-    if logging_level not in ['warn', 'warning']:
-        if logging_level == "critical":
-            logging.set_verbosity_critical()
-        elif logging_level == "error":
-            logging.set_verbosity_error()
-        elif logging_level == "info":
-            logging.set_verbosity_info()
-        elif logging_level == "debug":
-            logging.set_verbosity_debug()
-        else:
-            print("Warning: Logging level {l} is not an option.".format(l=logging_level))
-            print("\tUse one of: ")
-
-    if tokenizer_parallelism:
-        os.environ["TOKENIZERS_PARALLELISM"] = "true"
-    else:
-        os.environ["TOKENIZERS_PARALLELISM"] = "false"
+    set_logging_level(logging_level)
+    set_tokenizer_parallelism(tokenizer_parallelism)
+    device, device_num = get_device(device)
 
     device = device.lower()
-    if not device.startswith('cpu') and not device.startswith('gpu'):
-        print("device must be 'cpu', 'gpu', or of the form 'gpu:k'")
+    if not device.startswith('cpu') and not device.startswith('gpu') and not device.startswith('cuda'):
+        print("device must be 'cpu', 'gpu', 'cuda', or of the form 'gpu:k' or 'cuda:k'")
         print("\twhere k is an integer value for the device")
         print("Trying CPUs")
         device = 'cpu'
 
-    if "megatron-bert" in model:
-        try:
-            from transformers import BertTokenizer, MegatronBertForMaskedLM
-        except:
-            print("WARNING: You must install transformers>4.10 to use MegatronBertForMaskedLM")
-            print("\tPlease try another model.")
-            sys.exit()
-
-        config = AutoConfig.from_pretrained(model, output_hidden_states=True)
-        if "megatron-bert-cased" in model:
-            tokenizer = BertTokenizer.from_pretrained('nvidia/megatron-bert-cased-345m')
-        else:
-            tokenizer = BertTokenizer.from_pretrained('nvidia/megatron-bert-uncased-345m')
-        transformer_model = MegatronBertForMaskedLM.from_pretrained(model, config=config)
-    else:
-        config = AutoConfig.from_pretrained(model, output_hidden_states=True)
-        tokenizer = AutoTokenizer.from_pretrained(model)
-        transformer_model = AutoModel.from_pretrained(model, config=config)
+    config, tokenizer, transformer_model = get_model(model)
+    device, device_num = get_device(device)
 
     if device != 'cpu':
-        attached = False
-        if torch.cuda.is_available():
-            if device == 'gpu':
-                for device_num in range(0,torch.cuda.device_count()):
-                    try:
-                        transformer_model.to(device="cuda:{device_num}".format(device_num=device_num))
-                        attached = True
-                        break
-                    except:
-                        continue
-            else: # assign to specific gpu device number
-                device_num = int(device.split(":")[-1])
-                try:
-                    transformer_model.to(device="cuda:{device_num}".format(device_num=device_num))
-                    attached = True
-                except:
-                    pass
-        if not attached:
-            print("Unable to use CUDA (GPU), using CPU")
-            device = "cpu"
+        transformer_model.to(device="cuda:{device_num}".format(device_num=device_num))
 
     max_tokens = tokenizer.max_len_sentences_pair
 
-    ##Check and Adjust Input Typs
+    # check and adjust input types
     if not isinstance(text_strings, list):
         text_strings = [text_strings]
 
