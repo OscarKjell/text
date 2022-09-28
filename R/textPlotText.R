1759971f731e635881a962155dd5f2ed09d0c973
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2022-09-28 11:35:03 +0200

    an easy updates

---
+++ R/textPlotText.R
@@ -0,0 +1,27 @@
+#### textProjectText ####
+
+#' Function to calculate the highlight color value.
+#' @return List of names of models and tibbles
+#' @examples
+#' \dontrun{
+#' textProjectionText()
+#' }
+#' @seealso see \code{\link{textProjectionText}}
+#' @export
+textProjectionText <- function(aaa){
+    print("Hi!")
+}
+
+#### textPlotText ####
+
+#' Function to plot the highlight color value into semantic cloud.
+#' @return The RObject of the plot
+#' @examples
+#' \dontrun{
+#' textPlotText()
+#' }
+#' @seealso see \code{\link{textPlotText}}
+#' @export
+textPlotText <- function(aaa){
+    print("Hi again!")
+}
