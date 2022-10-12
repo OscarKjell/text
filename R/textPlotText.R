cf84fc7e1933570288c50867dc49d9b0cd64cf62
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2022-10-12 21:22:03 +0200

    Update some code of textProjectionText.

--- R/textPlotText.R
+++ R/textPlotText.R
@@ -13,8 +13,8 @@
 #' @return The RObject of the tokenizer.
 #' @noRd
 getTokenizer <- function(modelName){
-    tokenizers <- import("transformers")
-    autoTokenizers <- tokenizers$AutoTokenizer
+    transformerPack <- reticulate::import("transformers")
+    autoTokenizers <- transformerPack$AutoTokenizer
     return (autoTokenizers$from_pretrained(modelName))
 }
 
@@ -23,9 +23,9 @@ getTokenizer <- function(modelName){
 #' @param tokenizers A tokenizer model from getTokenizer.
 #' @return The transformed string.
 #' @noRd
-decodeToken <- function(aStringList, tokenizers){
-    ids <- aStringList %>% tokenizers$convert_tokens_to_ids()
-    return (ids %>% tokenizers$decode())
+decodeToken <- function(aStringList, tokenizerInUse){
+    ids <- aStringList %>% tokenizerInUse$convert_tokens_to_ids()
+    return (ids %>% tokenizerInUse$decode())
 }
 
 #' Split a text into sentences.
@@ -49,68 +49,132 @@ token2Sent_rowCLSSEP <- function(aTibble){
     names(rowCLSSEP) <- c("CLS", "SEP")
     return (rowCLSSEP)
 }
-#' Calculate the number of sentences.
-#' @param aTibble The input tibble of token embeddings.
-#' @param rowCLSSEP The row numbers of the tokens "[CLS]" and "[SEP]".
-#' @param sentTibble The blank sentence tibble.
-#' @return An integer of the number of sentences.
-#' @NoRd
-token2Sent_avgSentToken <- function(aTibble, rowCLSSEP, sentTibble){
-    
-    return (0)
-}
 #' Create a new sentence tibble in line with the token tibble.
 #' @param aTibble The input tibble of token embeddings.
 #' @return A new sentence tibble.
 #' @NoRd
 token2Sent_newSentTb <- function(aTibble){
     newSentTb <- aTibble %>% token2Sent_rowCLSSEP()
-    sentCol <- matrix(nrow=newSentTb %>% nrow(), ncol=1) %>% as.data.frame() %>% as_tibble()
+    sentCol <- matrix(nrow=newSentTb %>% nrow(), ncol=aTibble %>% ncol()) %>%
+     as.data.frame() %>% tibble::as_tibble()
     sentCol[1:nrow(newSentTb), 1] <- "new"
-    names(sentCol) <- c("sentences")
+    sentCol[1:nrow(newSentTb), 2:ncol(aTibble)] <- 0.0
+    names(sentCol)[1] <- c("sentences")
+    names(sentCol)[2:ncol(aTibble)] <- names(aTibble)[2:ncol(aTibble)]
     newSentTb <- cbind(newSentTb, sentCol)
     return (newSentTb)
 }
 #' Transform the tokens into a sentence cell in a single cell.
+#' @param newSentence The target sentence to transform and the output as well.
 #' @param rowCLS The row number of the token "[CLS]".
 #' @param rowSEP The row number of the token "[SEP]".
 #' @param aTibble The input tibble of token embeddings.
 #' @param tokenizers The tokenizer used.
+#' @param include_CLS_SEP To include the embeddings of "CLS", "SEP", "both", or "none".
 #' @return A sentence tibble.
 #' @NoRd
-token2Sent_getSentRowWise <- function(rowCLS, rowSEP, aTibble, tokenizers){
+token2Sent_getSentRowWise <- function(newSentence, rowCLS, 
+                                      rowSEP, aTibble, 
+                                      tokenizers, include_CLS_SEP){
+    if (include_CLS_SEP == "CLS"){
+        rowSEP <- rowSEP - 1
+    }else if(include_CLS_SEP == "SEP"){
+        rowCLS <- rowCLS + 1
+    }else if(include_CLS_SEP == "none"){
+        rowCLS <- rowCLS + 1
+        rowSEP <- rowSEP - 1
+    }else{include_CLS_SEP <- "both"}
     newSentence <- decodeToken(aTibble[["tokens"]][rowCLS:rowSEP], tokenizers)
     return (newSentence)
 }
-# TODO: continue code here, pmap
+#' Average the embedding values across tokens.
+#' @param newEmbed The input embedding value vector.
+#' @param rowCLS The row number of "[CLS]".
+#' @param rowSEP The row number of "[SEP]".
+#' @param aTibble The input tibble of token embeddings.
+#' @param include_CLS_SEP To include the embeddings of "CLS", "SEP", "both", or "none".
+#' @return An integer of the number of sentences.
+#' @NoRd
+token2Sent_avgSentTokenRowWise <- function(newEmbed, rowCLS,
+                                           rowSEP, aTibble,
+                                           include_CLS_SEP){
+    if (include_CLS_SEP == "CLS"){
+        rowSEP <- rowSEP - 1
+    }else if(include_CLS_SEP == "SEP"){
+        rowCLS <- rowCLS + 1
+    }else if(include_CLS_SEP == "none"){
+        rowCLS <- rowCLS + 1
+        rowSEP <- rowSEP - 1
+    }else{include_CLS_SEP <- "both"}
+    newEmbed <- aTibble[rowCLS:rowSEP, 2:ncol(aTibble)] %>% 
+        as.matrix() %>% colMeans() %>%
+        t() %>% tibble::as_tibble() 
+    names(newEmbed) <- names(aTibble)[2:ncol(aTibble)]
+    return (newEmbed)
+}
+# TODO: Preserve capital letters.
 #' Transform the tokens into sentence cells.
 #' @param aTibble The input tibble of token embeddings.
 #' @param tokenizers The tokenizer used.
-#' @importFrom purrr pmap
+#' @param include_CLS_SEP To include the embeddings of "CLS", "SEP", or "both".
+#' @importFrom furrr pmap
+#' @importFrom furrr pmap_dfr # future for paralell
+#' @importFrom future plan
+#' @importFrom future cluster
+#' @importFrom future future
+#' @importForm future value
 #' @return A sentence tibble.
 #' @NoRd
-token2Sent_getSentCell <- function(aTibble, tokenizers){
+token2Sent_getSent <- function(aTibble, tokenizers, include_CLS_SEP="both"){
 
+    # future::plan(future::cluster)
+    
     newSentTb <- token2Sent_newSentTb(aTibble)
-
-    return (0)
+    getSent_str <- future::future({
+        furrr::future_pmap(
+        list(
+            newSentTb[["sentences"]] %>% as.vector(),
+            newSentTb[["CLS"]] %>% as.vector(),
+            newSentTb[["SEP"]] %>% as.vector(),
+            aTibble %>% list(),
+            tokenizers %>% list(),
+            include_CLS_SEP %>% list()
+        ),
+        token2Sent_getSentRowWise
+        )
+    },seed=TRUE)
+    getSent_embed <- future::future({
+        furrr::future_pmap_dfr(
+        list(
+            newSentTb[,4:ncol(newSentTb)] %>% as.data.frame() %>% asplit(1),
+            newSentTb[["CLS"]] %>% as.vector(),
+            newSentTb[["SEP"]] %>% as.vector(),
+            aTibble %>% list(),
+            include_CLS_SEP %>% list()
+        ),
+        token2Sent_avgSentTokenRowWise
+        ) 
+    },seed=TRUE)
+    newSentTb[["sentences"]] <- future::value(getSent_str)
+    getSent_embed_ <- future::value(getSent_embed)
+    newSentTb <- cbind(newSentTb[,1:3], getSent_embed_)
+    return (newSentTb)
 }
+# TODO: continue to code here. Use func token2Sent_getSent to convert the token lists from textEmbed.
 #' Get the tibble of sentence embeddings.
-#' @param aTibble The input tibble of token embeddings.
-#' @param rowCLSSEP The row numbers of the token "[CLS]" and "[SEP]".
-#' @param sentTibble The blank sentence tibble.
+#' @param texts_embeddings The input tibble of token embeddings.
 #' @return An integer of the number of sentences.
 #' @NoRd
 token2Sent <- function(texts_embeddings){
 
     rowNum <- length(texts_embeddings$tokens$texts)
-    for (rowNo in rowNum %>% seq_len){
-        idCLSSEP <- texts_embeddings[["tokens"]][["texts"]][rowNo] %>% 
-        texts_embeddings[["sentences"]][["texts"]][rowNo]
-    }
+    # for (rowNo in rowNum %>% seq_len){
+        # idCLSSEP <- texts_embeddings[["tokens"]][["texts"]][rowNo] %>% 
+        # texts_embeddings[["sentences"]][["texts"]][rowNo]
+    #}
     # temp_[["sentences"]][["texts"]][1] <- list("a")
     # temp_[["sentences"]][2] <- list
-    texts_embeddings
+    # texts_embeddings
 
     return (0)
 }
@@ -147,7 +211,7 @@ textProjectionText <- function(
     x,
     y = NULL,
     model = "bert-base-uncased",
-    include_CLS_SEP = "both"
+    include_CLS_SEP = "both",
     lang_level = "all",
     projection_method = "ridge",
     device = "cpu",
