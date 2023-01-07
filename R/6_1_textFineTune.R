ed7ab38f96990112dbad638af1b183e4c78eb646
Oscar Kjell <oscar_kjell@hotmail.com>
2023-01-07 09:53:05 +0100

    include additional parameters in textFineTunTask() in R

--- R/6_1_textFineTune.R
+++ R/6_1_textFineTune.R
@@ -7,8 +7,6 @@
 # where to default save output? (cld include it in .cash -- get() )
 # argument to name model
 
-
-
 # OK: Stratify sampling!
 
 #textEmbed("hello",
@@ -23,15 +21,34 @@
 #reticulate::source_python("/Users/oscarkjell/Desktop/1 Projects/0 Research/0 text r-package/text/inst/python/huggingface_Interface4.py")
 
 
-# Task Adapted Pre-Training
+
+#' Task Adapted Pre-Training (experimental)
 #' @param text_outcome_data A dataframe, where the first column contain text data,
 #' and the second column the to-be-predicted variable (numeric or categorical).
-#' @param model (string) Path to the foundation model to use.
+#' @param model_name_or_path (string) Path to foundation/pretrained model or model identifier from huggingface.co/models
 #' @param output_dir (string) Path to the output directory.
 #' @param validation_proportion (Numeric) Proportion of the text_outcome_data to be used for validation.
 #' @param evaluation_proportion (Numeric) Proportion of the text_outcome_data to be used for evaluation.
 #' @param is_regression (Boolean) TRUE for regression tasks, FALSE for classification.
+#' @param config_name (String) Pretrained config name or path if not the same as model_name.
+#' @param tokenizer_name (String) Pretrained tokenizer name or path if not the same as model_name
+#' @param max_seq_length (Numeric) The maximum total input sequence length after tokenization. Sequences longer
+#' than this will be truncated, sequences shorter will be padded.
+#' @param evaluation_strategy (String or IntervalStrategy) — The evaluation strategy to adopt during training.
+#' Possible values are:
+#' "no": No evaluation is done during training.
+#' "steps": Evaluation is done (and logged) every eval_steps.
+#' "epoch": Evaluation is done at the end of each epoch.
+#' @param eval_accumulation_steps (Integer) Number of predictions steps to accumulate the output tensors for,
+#' before moving the results to the CPU. If left unset, the whole predictions are accumulated on GPU/TPU
+#' before being moved to the CPU (faster but requires more memory).
+#' @param num_train_epochs (Numeric) Total number of training epochs to perform
+#' (if not an integer, will perform the decimal part percents of the last epoch before stopping training).
+#' @param past_index (Numeric, defaults to -1) Some models like TransformerXL or XLNet can make use of the past hidden states
+#' for their predictions. If this argument is set to a positive int, the Trainer will use the corresponding output
+#' (usually index 2) as the past state and feed it to the model at the next training step under the keyword argument mems.
 #' @param set_seed (Numeric) Set the seed
+#' @param ... Parameters related to the fine tuning set in text-package file inst/python/arg2.json.
 #' @return A folder containing the pretrained model and output data. The model can then be used, for example, by
 #' textEmbed() by providing the model parameter with a the path to the output folder.
 #' @examples
@@ -39,14 +56,25 @@
 #' textFineTuneTask(text_outcome_data)
 #' }
 #' @seealso see \code{\link{textEmbed}}, \code{\link{textEmbed}}
+#' @details Information about more parameters see inst/python/args2.json (https://github.com/OscarKjell/text/tree/master/inst/python/args2.json).
+# Descriptions of settings can be found in inst/python/task_finetune.py under "class ModelArguments" and class DataTrainingArguments as well as
+# online at https://huggingface.co/docs/transformers/main_classes/trainer.
 #' @export
 textFineTuneTask <- function(text_outcome_data,
-                             model = "bert-base-uncased", # Also how to find my previously created one?
+                             model_name_or_path = "bert-base-uncased", # Also how to find my previously created one?
                              output_dir = "./runs/trial",
                              validation_proportion = 0.10,
                              evaluation_proportion = 0.10,
                              is_regression = TRUE,
-                             set_seed = 2022
+                             config_name = NULL,
+                             tokenizer_name = NULL,
+                             max_seq_length = 128L,
+                             evaluation_strategy = "epoch",
+                             eval_accumulation_steps = NULL,
+                             num_train_epochs = 3,
+                             past_index = -1,
+                             set_seed = 2022,
+                             ...
                              ){
 
   T1 <- Sys.time()
@@ -61,7 +89,7 @@ textFineTuneTask <- function(text_outcome_data,
 
   colnames(text_outcome_data) <-  c("text", "label")
 
-  # Data set partitioning help(sample)
+  # Data set partitioning
   props <- sample(c("train", "validation", "evaluation"),
                size=nrow(text_outcome_data),
                replace=TRUE,
@@ -76,13 +104,21 @@ textFineTuneTask <- function(text_outcome_data,
   text_outcome_df_val  = text_outcome_data[props=="validation", ]
   text_outcome_df_test = text_outcome_data[props=="evaluation", ]
 
-  # /Users/oscarkjell/Desktop/1 Projects/0 Research/0 text r-package/text/
   hgTransformerFineTune(json_path = "inst/python/args2.json",
+                        model_name_or_path = model_name_or_path,
+                        output_dir = output_dir,
                         text_outcome_df = text_outcome_df,
                         text_outcome_df_val = text_outcome_df_val,
                         text_outcome_df_test = text_outcome_df_test,
-                        is_regression = is_regression)
-
+                        is_regression = is_regression,
+                        config_name = config_name,
+                        tokenizer_name = tokenizer_name,
+                        max_seq_length = max_seq_length,
+                        evaluation_strategy = evaluation_strategy,
+                        eval_accumulation_steps = eval_accumulation_steps,
+                        num_train_epochs = num_train_epochs,
+                        #past_index = past_index,
+                        ...)
 
   T2 <- Sys.time()
   T2-T1
