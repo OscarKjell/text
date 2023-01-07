196d6d73b6d85a5c8ba9c3e80f8556d0e320612d
Oscar Kjell <oscar_kjell@hotmail.com>
2023-01-07 10:02:39 +0100

    install scikit-learn in the standard installation.

--- R/0_0_text_install.R
+++ R/0_0_text_install.R
@@ -56,10 +56,10 @@ textrpp_install <- function(conda = "auto",
   # Set system specific default versions
   if (rpp_version[[1]] == "rpp_version_system_specific_defaults") {
     if (is_osx() | is_linux()) {
-      rpp_version <- c("torch==1.11.0", "transformers==4.19.2", "numpy", "nltk")
+      rpp_version <- c("torch==1.11.0", "transformers==4.19.2", "numpy", "nltk", "scikit-learn")
     }
     if (is_windows()) {
-      rpp_version <- c("torch==1.11.0", "transformers==4.19.2", "numpy", "nltk")
+      rpp_version <- c("torch==1.11.0", "transformers==4.19.2", "numpy", "nltk", "scikit-learn")
     }
   }
 
