aac75fcfed86195b75e945699980ff6db37f85ab
Oscar Kjell <oscar_kjell@hotmail.com>
2023-02-05 11:15:00 +0100

    trying tests with a smaller distill model.

--- tests/testthat/test_6_finetune.R
+++ tests/testthat/test_6_finetune.R
@@ -8,20 +8,23 @@ context("Testing tasks")
 
 test_that("Task Fine-tuning tests", {
   skip_on_cran()
-
+textModels()
+  help("textFineTuneTask")
   unlink("./run_reg", recursive = TRUE)
   text::textFineTuneTask(Language_based_assessment_data_8[,c("satisfactiontexts", "hilstotal")],
-                   is_regression = TRUE,
-                   output_dir = "./run_reg")
+                         model_name_or_path = "distilroberta-base",
+                         is_regression = TRUE,
+                         output_dir = "./run_reg")
 
   # remove the folder
   unlink("./run_reg", recursive = TRUE)
 
   unlink("./run_clf", recursive = TRUE)
   text::textFineTuneTask(Language_based_assessment_data_8[,c("satisfactiontexts", "gender")],
-                   is_regression = FALSE,
-                   output_dir = "./run_clf",
-                   label_names = c("male", "female"))
+                         model_name_or_path = "distilroberta-base",
+                         is_regression = FALSE,
+                         output_dir = "./run_clf",
+                         label_names = c("male", "female"))
 
   # remove the folder
   unlink("./run_clf", recursive = TRUE)
