1317750087092e0a2ba2f1f70370715245919cf8
Adithya V Ganesan <v.adithyaganesan@gmail.com>
2023-10-02 08:20:15 -0400

    minor changes to run_mlm and test file

--- inst/python/run_mlm_test.py
+++ inst/python/run_mlm_test.py
@@ -1,20 +1,21 @@
 from huggingface_Interface4 import hgTransformerMLM
 import datasets
-import pandas as pd
 
 ##########################################################################
 ### Test 1: MLM Task #####################################################
 ##########################################################################
 
-def basic_mlm_test():
+def basic_mlm_test(**kwargs):
     """
     Basic test for checking if the MLM training feature works
     """
     data = datasets.load_dataset("rotten_tomatoes")
-    train_df = pd.DataFrame(data['train'])
-    val_df = pd.DataFrame(data['validation'])
-    test_df = pd.DataFrame(data['test'])
-    json_path = './mlm_args.json'
-    hgTransformerMLM(json_path, train_df, val_df, test_df)
+    train_df = data['train'].to_pandas()
+    val_df = data['validation'].to_pandas()
+    test_df = data['test'].to_pandas()
+    json_path = './mlm_args2.json'
+    hgTransformerMLM(json_path, train_df, val_df, test_df, **kwargs)
     return 
-    
+
+
+# kwargs: output_dir="/data/aviricnhipur/text_dummy_run/models/", seed=2023, do_train=True, do_eval=True, evaluation_strategy="epoch"    
