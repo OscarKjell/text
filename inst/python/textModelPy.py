a5802ab8e7192b8a5d171318866d7cba6cbd8493
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2023-12-25 12:18:50 +0100

    Update textModelPy.py

--- inst/python/textModelPy.py
+++ inst/python/textModelPy.py
@@ -115,15 +115,15 @@ def textModelsRMPy(target="default"):
 # main for test
 if __name__ == '__main__':
 
-     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-     model = AutoModelForMaskedLM.from_pretrained("bert-base-uncased")
+     tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+     model = AutoModelForMaskedLM.from_pretrained("distilbert-base-uncased")
 
      # Show the downloaded model.
      temp = textModelsPy()
      for a in temp:
          print(a)
 
-     textModelsRMPy(target="bert-base-uncased")
+     textModelsRMPy(target="distilbert-base-uncased")
 
      # Show the model again after deleting.
      temp = textModelsPy()
