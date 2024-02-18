41d5d55a36300e8c965d6f331e056967778ff81e
Oscar Kjell <oscarkjell@Oscars-MacBook-Pro.local>
2024-02-18 20:42:01 +0100

    Show testthat output and testing more in testTrainN.

--- tests/testthat/test_2_4_textTrainN.R
+++ tests/testthat/test_2_4_textTrainN.R
@@ -13,7 +13,35 @@ context("textTrainN")
 test_that("training with only x_append (without word embeddings)", {
   skip_on_cran()
 
-  # help("textTrainN")
+
+  # n_cross_val = 1 help(textTrainN)
+  tibble_to_plot_1 <- textTrainN(
+    x = word_embeddings_4$texts[c("satisfactiontexts", "harmonytexts")],
+    y = Language_based_assessment_data_8$hilstotal,
+    sample_percents = c(25, 50),
+    n_cross_val = 1,
+    handle_word_embeddings = "concatenate"
+  )
+
+  testthat::expect_that(tibble_to_plot_1, testthat::is_a("tbl"))
+
+
+  testthat::expect_equal(tibble_to_plot_1$Test1[[1]], -0.2393972, tolerance = 0.0001)
+  testthat::expect_equal(tibble_to_plot_1$Test1[[2]], 0.1855261, tolerance = 0.0001)
+  testthat::expect_equal(tibble_to_plot_1$mean[[1]], -0.2393972, tolerance = 0.0001)
+
+
+  plot_object_1 <- textTrainNPlot(
+    tibble = tibble_to_plot_1,
+    n_cross_val = 3,
+    x_unit = "percent"
+  )
+
+  testthat::expect_equal(length(plot_object_1$layers), 3)
+  testthat::expect_true(ggplot2::is.ggplot(plot_object_1))
+
+
+  # n_cross_val = 2
   tibble_to_plot <- textTrainN(
     x = word_embeddings_4$texts$harmonytext,
     y = Language_based_assessment_data_8$hilstotal,
