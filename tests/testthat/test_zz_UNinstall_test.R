32b8096a25fbc2a3f866d9c921cf91f7f8390492
Oscar Kjell <oscarkjell@Oscars-MacBook-Pro.local>
2024-02-22 11:27:49 +0100

    uninstall in the last file.

--- tests/testthat/test_zz_UNinstall_test.R
+++ tests/testthat/test_zz_UNinstall_test.R
@@ -4,11 +4,12 @@ library(text)
 test_that("UNinstalling text", {
   skip_on_cran()
 
-
+  if (Sys.info()["sysname"] == "Darwin" | Sys.info()["sysname"] == "Windows") {
   text::textrpp_uninstall(
     prompt = FALSE,
     envname = "test_ok")
 
+  }
 
   x_ok <- 1
   expect_equal(x_ok, 1)
