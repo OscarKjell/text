3fc92ad4e2f87c206e3bc2cd86d2f7cb36b1935e
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2024-02-22 18:26:25 +0100

    Update 1_1_textEmbed.R

--- R/1_1_textEmbed.R
+++ R/1_1_textEmbed.R
@@ -414,7 +414,7 @@ textEmbedRawLayers <- function(texts,
   ))
 
   if (is.numeric(layers)) {
-    if (max(layers) > textModelLayers(model)) {
+    if (max(layers) > textModelLayers(model, hg_gated, hg_token)) {
       stop("You are trying to extract layers that do not exist in this model.")
     }
   }
