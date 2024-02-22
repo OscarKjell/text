72b1d4e874e271e3ae28ce7c1cc42ffdb8f97ba0
Oscar Kjell <oscarkjell@Oscars-MacBook-Pro.local>
2024-02-22 08:46:50 +0100

    installing and uninstalling test for Mac and Windows

--- tests/testthat/test_0_install_test.R
+++ tests/testthat/test_0_install_test.R
@@ -4,20 +4,30 @@ library(text)
 test_that("installing text", {
   skip_on_cran()
 
-  text::textrpp_install(prompt = FALSE,
-                  envname = "test_ok")
+  # On Linux get  error at reticulate:::pip_install(...)
+  if (Sys.info()["sysname"] == "Darwin" | Sys.info()["sysname"] == "Windows") {
+    text::textrpp_install(prompt = FALSE,
+                          envname = "test_ok")
 
-  text::textrpp_initialize(textEmbed_test = TRUE,
-                     save_profile = TRUE,
-                     prompt = FALSE,
-                     condaenv = "test_ok")
+    text::textrpp_initialize(textEmbed_test = TRUE,
+                             save_profile = TRUE,
+                             prompt = FALSE,
+                             condaenv = "test_ok")
 
-  text_test <- text::textEmbed("hello")
+    text_test <- text::textEmbed("hello")
 
-  expect_equal(text_test$tokens$texts[[1]]$Dim1[[1]], -0.9554495, tolerance = 0.0001)
+    expect_equal(text_test$tokens$texts[[1]]$Dim1[[1]], -0.9554495, tolerance = 0.0001)
 
-  # help(textrpp_uninstall)
-#  textrpp_uninstall(prompt = FALSE,
-#                    envname = "test_ok")
+    # help(textrpp_uninstall)
+    text::textrpp_initialize(textEmbed_test = TRUE,
+                             save_profile = TRUE,
+                             prompt = FALSE,
+                             condaenv = "uninstall")
+
+    textrpp_uninstall(prompt = FALSE,
+                      envname = "uninstall")
+  }
+
+
 #
 })
