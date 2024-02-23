0e6b61005d74a6a355710ce438260b7658596439
CarlViggo <carlviggo@icloud.com>
2024-02-23 16:38:19 +0100

    bug resolved in textPredict

--- R/2_4_textPredict.R
+++ R/2_4_textPredict.R
@@ -30,25 +30,25 @@ textReturnModelAndEmbedding <- function(
     story_id = NULL) {
   # extend the timeout time for larger models.
   options(timeout = 5 * 60)
-
-
+  
+  
   # diaplay message to user
   cat(colourise("Loading model...", fg = "red"))
   cat("\n")
-
+  
   # extract model_name if its a url or filepath
   if (is.character(model_info)) {
     model_name <- basename(model_info)
     # find model in wd
     model_exists <- file.exists(model_name)
-
+    
     # determine how to load model
     if (grepl("github.com/", model_info) && isFALSE(model_exists) && isTRUE(save_model)) {
       # load from github
       loaded_model <- readRDS(url(model_info))
       # save model
       saveRDS(loaded_model, model_name)
-
+      
       # display message to user
       loaded_model_confirm <- paste0(c("The model:", model_name, "has been loaded and saved in:", getwd()), sep = "")
       cat(colourise(loaded_model_confirm, fg = "green"))
@@ -56,7 +56,7 @@ textReturnModelAndEmbedding <- function(
     } else if (grepl("github.com/", model_info) && isFALSE(model_exists) && isFALSE(save_model)) {
       # load from github, don't save
       loaded_model <- readRDS(url(model_info))
-
+      
       # display message to user
       loaded_model_confirm <- paste0(c("The model:", model_name, "has been loaded from:", model_info), sep = "")
       cat(colourise(loaded_model_confirm, fg = "green"))
@@ -64,7 +64,7 @@ textReturnModelAndEmbedding <- function(
     } else if (grepl("github.com/", model_info) && isTRUE(model_exists)) {
       # retrive model from wd if it's already downloaded
       loaded_model <- readRDS(model_name)
-
+      
       # display message to user
       loaded_model_confirm <- paste0(c("The model:", model_name, "has been loaded from:", getwd()), sep = "")
       cat(colourise(loaded_model_confirm, fg = "green"))
@@ -72,7 +72,7 @@ textReturnModelAndEmbedding <- function(
     } else {
       # load model from specific path (if it exists somewhere else than in the work directory)
       loaded_model <- readRDS(model_info)
-
+      
       # display message to user
       loaded_model_confirm <- paste0(c("The model:", model_name, "has been loaded from:", model_info), sep = "")
       cat(colourise(loaded_model_confirm, fg = "green"))
@@ -86,48 +86,48 @@ textReturnModelAndEmbedding <- function(
     cat(colourise(loaded_model_confirm, fg = "green"))
     cat("\n")
   }
-
+  
   # Check that both texts and word_embeddings aren't defined.
   if (!is.null(texts) && !is.null(word_embeddings)) {
     stop('Both arguments: "texts" and "word_embeddings" cannot be defined simultaneously. Choose one or the other.')
   }
-
+  
   ###### Create embeddings based on information stored in the pre-trained model ######
-
+  
   if (!is.null(texts) && is.null(word_embeddings)) {
     # Save default values for later use
     default_max_token_to_sentence <- 4
     default_aggregation_from_layers_to_tokens <- "concatenate"
     default_aggregation_from_tokens_to_texts <- "mean"
-
+    
     # Finds the line number for the line with "impute_missing_setting" (the line above the
     # line we're looking for, which is the model description)
     line_number <- grep("impute_missing_setting", loaded_model$model_description)
     new_line_number <- line_number + 1
-
+    
     # Extracts information about model type and layers.
     model_type <- extract_comment(loaded_model$model_description[new_line_number], "model")
     model_layers <- as.numeric(extract_comment(loaded_model$model_description[new_line_number], "layers"))
-
+    
     # Extracts the max_token_to_sentence, aggregation_from_layers_to_tokens,
     # and aggregation_from_tokens_to_texts from the model.
     input_string <- loaded_model$model_description[new_line_number]
-
+    
     max_token_to_sentence <- as.numeric(sub(".*max_token_to_sentence: (\\d+).*", "\\1", input_string))
-
+    
     aggregation_from_layers_to_tokens <- sub(".*aggregation_from_layers_to_tokens = (\\S+).*", "\\1", input_string)
-
+    
     aggregation_from_tokens_to_texts <- sub(".*aggregation_from_tokens_to_texts = (\\S+).*", "\\1", input_string)
-
+    
     # Check if the variables match input_string and assign the default values
     if (max_token_to_sentence == input_string) {
       max_token_to_sentence <- default_max_token_to_sentence
     }
-
+    
     if (aggregation_from_layers_to_tokens == input_string) {
       aggregation_from_layers_to_tokens <- default_aggregation_from_layers_to_tokens
     }
-
+    
     if (aggregation_from_tokens_to_texts == input_string) {
       aggregation_from_tokens_to_texts <- default_aggregation_from_tokens_to_texts
     }
@@ -147,20 +147,20 @@ textReturnModelAndEmbedding <- function(
   else if (!is.null(word_embeddings) && is.null(texts)) {
     embeddings <- word_embeddings
   }
-
+  
   ####### Special treatment for implicit motives ######
-
+  
   # Calculate the average of the current and the next word_embedding per story_id
   if (!is.null(story_id)) {
     T1_story_id <- Sys.time()
-
+    
     embeddings$texts$texts$story_id <- as.numeric(as.factor(story_id))
-
+    
     # Define a custom function to calculate the running average
     running_avg <- function(x) {
       c(x[1], (x[-1] + x[-length(x)]) / 2)
     }
-
+    
     # Apply the running average function to each embedding column by story_id
     embeddings$texts$texts <- dplyr::group_by(embeddings$texts$texts, story_id) %>%
       dplyr::mutate(
@@ -168,23 +168,23 @@ textReturnModelAndEmbedding <- function(
           dplyr::starts_with("Dim"),
           running_avg)) %>%
       dplyr::ungroup()
-
+    
     # Ungroup (remove the storyid column)
     embeddings$texts$texts <- dplyr::ungroup(embeddings$texts$texts)
-
+    
     T2_story_id <- Sys.time()
     Time_story_id <- T2_story_id - T1_story_id
     Time_story_id <- sprintf("Completed word-embedding concatenation per story-id. Duration: %f %s", Time_story_id, units(Time_story_id))
     cat(colourise(Time_story_id, fg = "green"))
     cat("\n")
   }
-
+  
   ##### End special treatment for automatic implicit motive coding #####
-
+  
   # store classes
   #classes <- loaded_model$final_recipe$levels$y$values
   classes <- loaded_model$final_model$pre$actions$recipe$recipe$orig_lvls$y$values
-
+  
   emb_and_model <- list(loaded_model = loaded_model, embeddings = embeddings, classes = classes)
   return(emb_and_model)
 }
@@ -306,25 +306,25 @@ textPredict <- function(model_info = NULL,
     stop('Both arguments: "texts" and "word_embeddings" cannot be defined simultaneously.
          Choose one or the other.')
   }
-
+  
   #### Special treatment for implicit motives - see private functions ####
-
+  
   # get_model_info retrieves the particular configurations that are needed for automatic implicit motive coding automatically
   get_model_info <- get_model_info(model_info,
                                    user_id,
                                    show_texts = show_texts,
                                    type = type,
                                    texts = texts)
-
+  
   model_info <- get_model_info$model_info
   show_texts <- get_model_info$show_texts
   show_prob <- get_model_info$show_prob
   type <- get_model_info$type
   texts <- get_model_info$texts
   user_id <- get_model_info$user_id
-
+  
   #### End Special treatment for implicit motives ####
-
+  
   #### Automatically extract embeddings that are compatible with the model ####
   if (!is.null(texts)) {
     # Retrieve embeddings that are compatible with the model.
@@ -337,19 +337,19 @@ textPredict <- function(model_info = NULL,
       device = device,
       story_id = story_id
     )
-
+    
     # Retrieve model from emb_and_mod object
     loaded_model <- emb_and_mod$loaded_model
-
+    
     # Retrieve embeddings from emb_and_mod object
     word_embeddings <- emb_and_mod$embeddings$texts
-
+    
     # Retrieve classes in case of logistic regression
     classes <- emb_and_mod$classes
   } else {
     loaded_model <- model_info
   }
-
+  
   # check if model is defined
   if (is.null(loaded_model)) {
     stop("No model was found.")
@@ -358,23 +358,23 @@ textPredict <- function(model_info = NULL,
   if (is.null(word_embeddings) && is.null(x_append)) {
     stop("No embeddings were found.")
   }
-
+  
   # Get the right word-embeddings
   if (dim_names == TRUE) {
     # Select the predictor variables needed for the prediction
     target_variables_names <- loaded_model$final_model$pre$actions$recipe$recipe$var_info$variable[
       loaded_model$final_model$pre$actions$recipe$recipe$var_info$role == "predictor"]
     ####    #target_variables_names <- loaded_model$final_recipe$var_info$variable[loaded_model$final_recipe$var_info$role == "predictor"]
-
+    
     ## Get Word Embedding Names
     # remove those starting with Dim0
     We_names1 <- target_variables_names[!grepl("^Dim0", target_variables_names)]
-
+    
     # Get everything after "_"
     We_names1_v_colnames <- substring(We_names1, regexpr("_", We_names1) + 1)
     # Get unique (keep the order the same)
     word_embeddings_names <- unique(We_names1_v_colnames)
-
+    
     # Select the word embeddings
     word_embeddings <- word_embeddings[word_embeddings_names]
   } else {
@@ -382,68 +382,68 @@ textPredict <- function(model_info = NULL,
     word_embeddings <- textDimName(word_embeddings,
                                    dim_names = FALSE
     )
-
+    
     word_embeddings_names <- "word_embeddings"
   }
-
+  
   if (!is.null(x_append)) {
     ### Sort a_append: select all Dim0 (i.e., x_append variables)
     dims0 <- target_variables_names[grep(
       "^Dim0",
       target_variables_names
     )]
-
+    
     # select everything after the first "_".
     variable_names <- substring(dims0, regexpr("_", dims0) + 1)
-
+    
     # Select those names from the "data"
     x_append_target <- x_append %>% dplyr::select(dplyr::all_of(variable_names))
   } else {
     variable_names <- NULL
     x_append_target <- NULL
   }
-
+  
   # Adding embeddings and x_append (if any)
   new_data1 <- sorting_xs_and_x_append(
     x = word_embeddings,
     x_append = x_append_target, ...
   )
   new_data1 <- new_data1$x1
-
-
+  
+  
   # Original structure:
   # Dealing with NAs
   # new_data1$id_nr <- c(seq_len(nrow(new_data1)))
   # new_data1 <- new_data1[complete.cases(new_data1), ]
   # new_data_id_nr_col <- tibble::as_tibble_col(seq_len(nrow(new_data1)), column_name = "id_nr")
   # Dealing with NAs # Position of new_data_id_nr_col and new_data1 has now been switched.
-
+  
   new_data1$id_nr <- c(seq_len(nrow(new_data1)))
   new_data_id_nr_col <- tibble::as_tibble_col(seq_len(nrow(new_data1)), column_name = "id_nr")
   new_data1 <- new_data1[complete.cases(new_data1), ]
-
-
+  
+  
   #### Load prepared_with_recipe
   #ok#  data_prepared_with_recipe <- recipes::bake(loaded_model$final_recipe, new_data1)
-
+  
   # Get column names to be removed from output
-#  colnames_to_b_removed <- colnames(data_prepared_with_recipe)
-#  colnames_to_b_removed <- colnames_to_b_removed[!colnames_to_b_removed == "id_nr"]
-
+  #  colnames_to_b_removed <- colnames(data_prepared_with_recipe)
+  #  colnames_to_b_removed <- colnames_to_b_removed[!colnames_to_b_removed == "id_nr"]
+  
   colnames_to_b_removed <- loaded_model$final_model$pre$actions$recipe$recipe$var_info$variable[
     loaded_model$final_model$pre$actions$recipe$recipe$var_info$role == "predictor"]
-
-
+  
+  
   # If the user has defined a threshold, then implement the threshold algorithm.
   if (!is.null(threshold)) {
     # Retrieves the two classes
     class1 <- classes[1]
     class2 <- classes[2]
-
+    
     # Create column names
     class1_col_name <- paste0(class1, "_prob")
     class2_col_name <- paste0(class2, "_prob")
-
+    
     # Predict
     predicted_scores2 <- new_data1 %>%
       dplyr::bind_cols(stats::predict(loaded_model$final_model, new_data = new_data1, type = "prob")) %>% # , ...
@@ -451,38 +451,38 @@ textPredict <- function(model_info = NULL,
       dplyr::full_join(new_data_id_nr_col, by = "id_nr") %>%
       dplyr::arrange(id_nr) %>%
       dplyr::select(-id_nr)
-
+    
     # Rename columns
     predicted_scores2 <- predicted_scores2 %>%
       dplyr::rename(
         !!class1_col_name := 1,
         !!class2_col_name := 2
       )
-
+    
     # If the user desires to only view class, then remove the probabilty columns.
     if (type == "class" && show_prob == FALSE) {
       predicted_scores2 <- predicted_scores2 %>%
         dplyr::mutate(predicted_class = ifelse(!!rlang::sym(class1_col_name) >= threshold, class1, class2)) %>%
         dplyr::select(predicted_class)
-
+      
       ############## CHANGE!!!!! #############
       we_names <- paste(word_embeddings_names, collapse = "_", sep = "")
       v_names <- paste(variable_names, collapse = "_", sep = "")
-
+      
       y_name <- loaded_model$model_description[3]
       y_name <- gsub("[[:space:]]", "", y_name)
       y_name <- gsub("y=", "", y_name)
-
+      
       colnames(predicted_scores2) <- paste(we_names, "_", v_names, "_", y_name, "pred", sep = "")
-
+      
       # If the user desires to view the classes and the probability columns.
     } else if (type == "class" && show_prob == TRUE) {
       predicted_scores2 <- predicted_scores2 %>%
         dplyr::mutate(predicted_class = ifelse(!!rlang::sym(class1_col_name) >= threshold, class1, class2)) %>%
         dplyr::select(predicted_class, dplyr::everything())
-
+      
     } else if (is.null(type)) {
-
+      
     }
   }
   # If no threshold is defined, then use the predefined threshold of 50%.
@@ -494,8 +494,8 @@ textPredict <- function(model_info = NULL,
     #      dplyr::full_join(new_data_id_nr_col, by = "id_nr") %>%
     #      dplyr::arrange(id_nr) %>%
     #      dplyr::select(-id_nr)
-
-
+    
+    
     ## OK ###
     predicted_scores2 <- new_data1 %>%
       dplyr::bind_cols(stats::predict(loaded_model$final_model, new_data = new_data1, type = type)) %>% # , ...
@@ -503,45 +503,45 @@ textPredict <- function(model_info = NULL,
       dplyr::full_join(new_data_id_nr_col, by = "id_nr") %>%
       dplyr::arrange(id_nr) %>%
       dplyr::select(-id_nr)
-
+    
     ## OK ###
-
+    
     #################### CHANGE
     we_names <- paste(word_embeddings_names, collapse = "_", sep = "")
     v_names <- paste(variable_names, collapse = "_", sep = "")
-
+    
     y_name <- loaded_model$model_description[3]
     y_name <- gsub("[[:space:]]", "", y_name)
     y_name <- gsub("y=", "", y_name)
-
+    
     colnames(predicted_scores2) <- paste(we_names, "_", v_names, "_", y_name, "pred", sep = "")
-
+    
     # If no threshold is defined, but both classification and prediction is to be viewed
     if (show_prob == TRUE) {
-      prob_scores <- data_prepared_with_recipe %>%
+      prob_scores <- new_data1 %>%
         dplyr::bind_cols(stats::predict(loaded_model$final_model, new_data = new_data1, type = "prob")) %>%
         dplyr::select(-!!colnames_to_b_removed) %>%
         dplyr::full_join(new_data_id_nr_col, by = "id_nr") %>%
         dplyr::arrange(id_nr) %>%
         dplyr::select(-id_nr)
-
+      
       # Adding probabilities to predicted_scores2
       predicted_scores2 <- cbind(predicted_scores2, prob_scores)
     }
   }
-
+  
   # Include text in predictions
   if (show_texts) {
     predicted_scores2 <- predicted_scores2 %>%
       dplyr::mutate(texts = texts)
   }
-
+  
   #### Implicit motives section, see private_functions #####
-
+  
   # Check for implicit motives configuration
   if (is.character(model_info)) {
     lower_case_model <- tolower(model_info)
-
+    
     if (
       grepl("power", lower_case_model) ||
       grepl("achievement", lower_case_model) ||
@@ -592,24 +592,24 @@ textPredictAll <- function(models,
                            x_append = NULL,
                            ...) {
   output_predictions <- list()
-
+  
   # If textTrain has created many models at the same time, select them from "all_output".
   if (!is.null(models$all_output)) {
     models <- models$all_output
   }
-
+  
   # Remove singlewords_we if it exist
   if (!is.null(word_embeddings$singlewords_we)) {
     word_embeddings$singlewords_we <- NULL
   }
-
+  
   for (i in seq_len(length(models))) {
     preds <- textPredict(
       model_info = models[[i]],
       word_embeddings = word_embeddings,
       x_append = x_append, ...
     )
-
+    
     output_predictions[[i]] <- preds
   }
   output_predictions1 <- dplyr::bind_cols(output_predictions)
@@ -674,11 +674,11 @@ textPredictTest <- function(y1,
     yhat1_absolut_error <- abs(yhat1 - y1)
     yhat1_absolut_error_mean <- mean(yhat1_absolut_error)
     yhat1_absolut_error_sd <- sd(yhat1_absolut_error)
-
+    
     yhat2_absolut_error <- abs(yhat2 - y1)
     yhat2_absolut_error_mean <- mean(yhat2_absolut_error)
     yhat2_absolut_error_sd <- sd(yhat2_absolut_error)
-
+    
     # T-test
     t_test_results <- stats::t.test(yhat1_absolut_error,
                                     yhat2_absolut_error,
@@ -698,13 +698,13 @@ textPredictTest <- function(y1,
     output <- list(descriptives, cohensD, t_test_results)
     names(output) <- c("Descriptives", "Effect_size", "Test")
   }
-
+  
   ## If comparing predictions from models that predict DIFFERENT outcomes
-
+  
   if (method == "bootstrap") {
     set.seed(seed)
     # Bootstrap data to create distribution of correlations; help(bootstraps)
-
+    
     # Correlation function
     if (statistic == "correlation") {
       stats_on_bootstrap <- function(split) {
@@ -714,8 +714,8 @@ textPredictTest <- function(y1,
         )
       }
     }
-
-
+    
+    
     # AUC function
     if (statistic == "auc") {
       stats_on_bootstrap <- function(split) {
@@ -725,42 +725,42 @@ textPredictTest <- function(y1,
         )
       }
     }
-
-
+    
+    
     # Creating correlation distribution for y1 and yhat1
     y_yhat1_df <- tibble::tibble(y1, yhat1)
     boots_y1 <- rsample::bootstraps(y_yhat1_df,
                                     times = bootstraps_times,
                                     apparent = FALSE
     )
-
+    
     boot_corrss_y1 <- boots_y1 %>%
       dplyr::mutate(corr_y1 = purrr::map(splits, stats_on_bootstrap))
-
-
-
+    
+    
+    
     boot_y1_distribution <- boot_corrss_y1 %>%
       tidyr::unnest(corr_y1) %>%
       dplyr::select(corr_y1)
-
+    
     # Creating correlation distribution for y2 and yhat2
     y_yhat2_df <- tibble::tibble(y2, yhat2)
     boots_y2 <- rsample::bootstraps(y_yhat2_df,
                                     times = bootstraps_times,
                                     apparent = FALSE
     )
-
+    
     boot_corrss_y2 <- boots_y2 %>%
       dplyr::mutate(corr_y2 = purrr::map(splits, stats_on_bootstrap))
-
+    
     boot_y2_distribution <- boot_corrss_y2 %>%
       tidyr::unnest(corr_y2) %>%
       dplyr::select(corr_y2)
-
-
+    
+    
     ### Examining the overlap
     x_list_dist <- list(boot_y1_distribution$corr_y1, boot_y2_distribution$corr_y2)
-
+    
     output <- overlapping::overlap(x_list_dist)
     output <- list(output$OV[[1]])
     names(output) <- "overlapp_p_value"
