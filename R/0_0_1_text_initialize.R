3897115b1eb52a2eaaedf70c79f7e52efe9790e4
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2024-09-05 12:06:51 +0200

    Update 0_0_1_text_initialize.R

--- R/0_0_1_text_initialize.R
+++ R/0_0_1_text_initialize.R
@@ -52,14 +52,6 @@ textrpp_initialize <- function(python_executable = NULL,
       reticulate::use_condaenv(settings$val, required = TRUE)
     }
   }
-
-  cat("\033[31m===============R code check 1111 is here!===============\033[0m\n")
-  # 20240903 R 4.4.1, using steps in the link to install (https://github.com/moomoofarm1/r-text_misc/blob/main/LocalSetupOfR_if_ANACONDA_incompatible.md)
-  # Replicate the error
- # ===============The code check 1111 is here!===============
-#OMP: Error #15: Initializing libomp.dylib, but found libomp.dylib already initialized.
-#OMP: Hint This means that multiple copies of the OpenMP runtime have been linked into the program. That is dangerous, since it can degrade performance or cause incorrect results. The best thing to do is to ensure that only a single OpenMP runtime is linked into the process, e.g. by avoiding static linking of the OpenMP runtime in any library. As an unsafe, unsupported, undocumented workaround you can set the environment variable KMP_DUPLICATE_LIB_OK=TRUE to allow the program to continue to execute, but that may cause crashes or silently produce incorrect results. For more information, please see http://openmp.llvm.org/
-#ERROR: loading failed
   
   # Importing this here may start importing necessary packages
   reticulate::source_python(system.file("python",
