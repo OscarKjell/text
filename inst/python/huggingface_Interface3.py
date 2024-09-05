084a1b4b49ca20ac778e73641776c41a9b7e9f98
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2024-09-05 12:08:15 +0200

    Update huggingface_Interface3.py

--- inst/python/huggingface_Interface3.py
+++ inst/python/huggingface_Interface3.py
@@ -22,8 +22,6 @@ from nltk.tokenize import sent_tokenize
 
 import os, sys
 
-print("\033[31mPython check 1111 is here!\033[0m")
-
 ACCEPTED_TASKS = ["text-classification", "sentiment-analysis", "question-answering", "translation", 
     "summarization", "token-classification", "ner", "text-generation", "zero-shot-classification"]
 
