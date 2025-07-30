dd2d42efa0f3910525ad7b454be7cde54c418794
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2025-07-30 06:21:05 +0200

    Update 2_4_1_textPredictTextTrained.R

--- R/2_4_1_textPredictTextTrained.R
+++ R/2_4_1_textPredictTextTrained.R
@@ -1031,7 +1031,7 @@ textPredictAll <- function(models,
 #' @importFrom rsample analysis bootstraps
 #' @importFrom yardstick roc_auc_vec
 #' @export
-textPredictTest <- function(
+textPredictTest <- <- function(
     y1,
     y2,
     yhat1,
@@ -1044,7 +1044,8 @@ textPredictTest <- function(
     seed = 42,
     ...) {
   
-  y2 = y1
+  if (is.null(y2) && !is.null(y1)) y2 = y1
+  if (!is.null(y2) && is.null(y1)) y1 = y2
   set.seed(seed)
   
   if(method == "bootstrap"){
